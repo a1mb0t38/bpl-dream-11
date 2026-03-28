ed22f8e19f9e9411440fc5675d79f4d308ad89c7
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-03-28 16:21:29 +0600

    added the nav bar,banner and bring data from json public folder

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import dollarImg from  "../../assets/dollar 1.png";
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none">
+                    <button className="flex justify-between items-center gap-2 font-bold text-xl">
+                        0 coin
+                        <img src={dollarImg} alt="dollar Image" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
